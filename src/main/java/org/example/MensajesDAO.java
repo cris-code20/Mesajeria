50f6b15cc685c2a6e32f9789cea31d6b07f4dcb5
cris <cris.cadosh@gmail.com>
2023-08-23 18:32:48 -0400

    Agregamos la fucionalidad de crear y listar los datos

--- src/main/java/org/example/MensajesDAO.java
+++ src/main/java/org/example/MensajesDAO.java
@@ -1,13 +1,62 @@
 package org.example;
 
+import com.mysql.cj.xdevapi.Result;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MensajesDAO {
 
     public static void crearMensajeDB(Mensajes mensajes){
+        Conexion db_connect = new Conexion();
+
+        try(Connection conexion = db_connect.get_connection()) {
+
+            PreparedStatement ps = null;
+
+            try {
+                String query = "INSERT INTO mensajes ( mensaje, autor_mensaje) VALUES (?,?);";
+                ps=conexion.prepareStatement(query);
+                ps.setString(1,mensajes.getMensaje());
+                ps.setString(2,mensajes.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("mensaje creado correctamente");
+
+            }catch (SQLException ex){
+                System.out.println(ex);
+            }
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }
 
     }
 
     public  static void  leerMensajesDB(){
+        Conexion db_connection = new Conexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
+         try (Connection conexion = db_connection.get_connection()){
+
+                 String query = "SELECT * FROM mensajes";
+                 ps=conexion.prepareStatement(query);
+                 rs=ps.executeQuery();
+
+                 while (rs.next()){
+                     System.out.println("ID: " + rs.getInt("id_mesaje"));
+                     System.out.println("MENASAJE: " +rs.getString("mensaje"));
+                     System.out.println("AUTHOR: " +rs.getString("autor_mensaje"));
+                     System.out.println("DATE: " +rs.getString("fecha_mensaje"));
+                     System.out.println("");
+                 }
+
+         }catch (SQLException e){
+             System.out.println("error al listar mensaje");
+             System.out.println(e);
+         }
     }
 
     public static void borrarMensajesDB(int id_mensaje){
--- src/main/java/org/example/MensajesService.java
+++ src/main/java/org/example/MensajesService.java
@@ -1,13 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class MensajesService {
 
     public static void crearUnMensaje(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("escribe tu mensaje");
+        String texto = sc.nextLine();
+        System.out.println("tu nombre");
+        String nombre = sc.nextLine();
+
+        Mensajes mensajes = new Mensajes();
+        mensajes.setMensaje(texto);
+        mensajes.setAutor_mensaje(nombre);
+
+        MensajesDAO.crearMensajeDB(mensajes);
 
     }
 
     public  static void  leerMensajes(){
-
+        MensajesDAO.leerMensajesDB();
     }
 
     public static void borrarMensajes(){
